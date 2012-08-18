ad04e1c2132441b28b801587ab49f9cf28f54edb
Mitchell Dorrell <FutureHypoon@gmail.com>
2012-08-17 22:38:34 -0400

    Huge update: added elements, added front-side signals.
    Removed offset variables in favor of variables representing the center
    of the figure.
    User variables written as inches multiplied by 25.4.
    Added variables for element ring locations, both for LEDs and headers.
    Removed commented code that has already been re-implemented in a
    superior way.
    Added some comments describing sections of the document.

--- hardware/scripts/watchxml.py
+++ hardware/scripts/watchxml.py
@@ -1,10 +1,31 @@
 from xml.etree.ElementTree import Element,SubElement,ElementTree,dump
 from xml.etree import ElementTree as ET
 from xml.dom import minidom
+from math import cos,sin,tan,atan,asin,acos,pi
 
-DIAMETER = 52.07
-XOFFSET = 12.065
-YOFFSET = 12.065
+# Diameter of PCB
+DIAMETER = 2.05*25.4
+# Location of center of PCB
+ORIGINX=1.5*25.4
+ORIGINY=1.5*25.4
+
+# Imaginary innermost ring of LEDs
+R0=0.35*25.4
+# Inside ring of LEDs
+R1=0.5*25.4
+# Middle ring of LEDs
+R2=0.65*25.4
+# Outside ring of LEDs
+R3=0.8*25.4
+# Imaginary outermost ring of LEDs
+R4=0.95*25.4
+
+# Inside ring of connectors
+HRI = 0.325*25.4
+# Outside ring of connectors
+HRO = 23.8125
+### NOTE: above value is provided in MILLIMETERS, and therefore does not have the multiplication by 25.4 ###
+
 
 def prettify(elem):
 	rough_string = ET.tostring(elem, 'utf-8')
@@ -62,35 +83,36 @@ grid.set("altunitdist","inch")
 grid.set("altunit","inch")
 
 layers = ET.parse('layers.brd').getroot()
-#for i in list(layers):
-#	i.tail=''
-#layers.tail=''
-#layers.text=''
 striptextandtail(layers)
 
 drawing.append(layers)
 
 board = SubElement(drawing,"board")
 plain = SubElement(board,"plain")
+
+### Board dimensions defined below ###
+
 addmaxboard(plain)
 
 lowcurve = SubElement(plain,"wire")
-lowcurve.set("x1",str(XOFFSET))
-lowcurve.set("x2",str(XOFFSET+DIAMETER))
-lowcurve.set("y1",str(DIAMETER/2+YOFFSET))
-lowcurve.set("y2",str(DIAMETER/2+YOFFSET))
+lowcurve.set("x1",str(ORIGINX-DIAMETER/2))
+lowcurve.set("x2",str(ORIGINX+DIAMETER/2))
+lowcurve.set("y1",str(ORIGINY))
+lowcurve.set("y2",str(ORIGINY))
 lowcurve.set("width","0")
 lowcurve.set("layer","20")
 lowcurve.set("curve","180")
 
 highcurve = SubElement(plain,"wire")
-highcurve.set("x1",str(XOFFSET))
-highcurve.set("x2",str(XOFFSET+DIAMETER))
-highcurve.set("y1",str(DIAMETER/2+YOFFSET))
-highcurve.set("y2",str(DIAMETER/2+YOFFSET))
+highcurve.set("x1",str(ORIGINX-DIAMETER/2))
+highcurve.set("x2",str(ORIGINX+DIAMETER/2))
+highcurve.set("y1",str(ORIGINY))
+highcurve.set("y2",str(ORIGINY))
 highcurve.set("width","0")
 highcurve.set("layer","20")
 highcurve.set("curve","-180")
+
+### Board dimensions defined above ###
 
 libraries = ET.parse('libraries.brd').getroot()
 striptextandtail(libraries)
@@ -109,7 +131,255 @@ board.append(autorouter)
 
 elements = SubElement(board,"elements")
 
+### Elements defined below ###
+
+for i in range(0,60):
+	j = SubElement(elements,"element")
+	j.set("name","LED"+str(i+1))
+	j.set("library","Customs")
+	j.set("package","LED-0603")
+	j.set("value","RED")
+	j.set("x",str(ORIGINX+R1*cos((pi/2)-i*pi/30)))
+	j.set("y",str(ORIGINY+R1*sin((pi/2)-i*pi/30)))
+	j.set("rot","R"+str(((360-i*6)+180*(i%2))%360))
+
+for i in range(0,60):
+	j = SubElement(elements,"element")
+	j.set("name","LED"+str(i+61))
+	j.set("library","Customs")
+	j.set("package","LED-0603")
+	j.set("value","GREEN")
+	j.set("x",str(ORIGINX+R2*cos((pi/2)-i*pi/30)))
+	j.set("y",str(ORIGINY+R2*sin((pi/2)-i*pi/30)))
+	j.set("rot","R"+str(((360-i*6)+180*(i%2))%360))
+
+for i in range(0,60):
+	j = SubElement(elements,"element")
+	j.set("name","LED"+str(i+121))
+	j.set("library","Customs")
+	j.set("package","LED-0603")
+	j.set("value","BLUE")
+	j.set("x",str(ORIGINX+R3*cos((pi/2)-i*pi/30)))
+	j.set("y",str(ORIGINY+R3*sin((pi/2)-i*pi/30)))
+	j.set("rot","R"+str(((360-i*6)+180*(i%2))%360))
+
+for i in range(0,6):
+	j = SubElement(elements,"element")
+	j.set("name","H"+str(i+1))
+	j.set("library","Customs")
+	j.set("package","3X2_1MM_SMD")
+	j.set("value","2X3")
+	j.set("x",str(ORIGINX+HRI*cos((pi/2)-i*pi/3)))
+	j.set("y",str(ORIGINY+HRI*sin((pi/2)-i*pi/3)))
+	j.set("rot","MR"+str(i*60))
+
+for i in range(0,3):
+	j = SubElement(elements,"element")
+	j.set("name","H"+str(i+7))
+	j.set("library","Customs")
+	j.set("package","2X2_1MM_SMD")
+	j.set("value","2X2")
+	j.set("x",str(ORIGINX+HRO*cos((pi/2)-i*2*pi/3)))
+	j.set("y",str(ORIGINY+HRO*sin((pi/2)-i*2*pi/3)))
+	j.set("rot","MR"+str((180+i*120)%360))
+
+### Elements defined above ###
+
+### Signals defined below ###
+
 signals = SubElement(board,"signals")
+
+for i in range(0,30):
+	j = SubElement(signals,"signal")
+	j.set("name","N$"+str(i+1))
+	k = SubElement(j,"contactref")
+	k.set("element","LED"+str(2*i+1))
+	k.set("pad","A")
+	l = SubElement(j,"via")
+	l.set("x",str(ORIGINX+0.5*(R0+R1)*cos(pi/2-2*i*pi/30-pi/60)))
+	l.set("y",str(ORIGINY+0.5*(R0+R1)*sin(pi/2-2*i*pi/30-pi/60)))
+	l.set("extent","1-16")
+	l.set("drill","0.635")
+	m = SubElement(j,"contactref")
+	m.set("element","LED"+str(2*i+2))
+	m.set("pad","C")
+	
+	n = SubElement(j,"wire")
+	n.set("x1",str(ORIGINX+(R1-0.75)*cos((pi/2)-2*i*pi/30)))
+	n.set("y1",str(ORIGINY+(R1-0.75)*sin((pi/2)-2*i*pi/30)))
+	n.set("x2",str(ORIGINX+0.5*(R0+R1)*cos(pi/2-2*i*pi/30-pi/60)))
+	n.set("y2",str(ORIGINY+0.5*(R0+R1)*sin(pi/2-2*i*pi/30-pi/60)))
+	n.set("width","0.4064")
+	n.set("layer","1")
+	
+	o = SubElement(j,"wire")
+	o.set("x1",str(ORIGINX+(R1-0.75)*cos((pi/2)-(2*i+1)*pi/30)))
+	o.set("y1",str(ORIGINY+(R1-0.75)*sin((pi/2)-(2*i+1)*pi/30)))
+	o.set("x2",str(ORIGINX+0.5*(R0+R1)*cos(pi/2-2*i*pi/30-pi/60)))
+	o.set("y2",str(ORIGINY+0.5*(R0+R1)*sin(pi/2-2*i*pi/30-pi/60)))
+	o.set("width","0.4064")
+	o.set("layer","1")
+
+for i in range(0,30):
+	j = SubElement(signals,"signal")
+	j.set("name","N$"+str(i+31))
+	k = SubElement(j,"contactref")
+	k.set("element","LED"+str(2*i+62))
+	k.set("pad","C")
+	l = SubElement(j,"via")
+	l.set("x",str(ORIGINX+0.5*(R1+R2)*cos(pi/2-2*i*pi/30-pi/30-pi/60)))
+	l.set("y",str(ORIGINY+0.5*(R1+R2)*sin(pi/2-2*i*pi/30-pi/30-pi/60)))
+	l.set("extent","1-16")
+	l.set("drill","0.635")
+	m = SubElement(j,"contactref")
+	m.set("element","LED"+str((2*i+3)%60+60))
+	m.set("pad","A")
+	
+	n = SubElement(j,"wire")
+	n.set("x1",str(ORIGINX+(R2-0.75)*cos((pi/2)-(2*i+1)*pi/30)))
+	n.set("y1",str(ORIGINY+(R2-0.75)*sin((pi/2)-(2*i+1)*pi/30)))
+	n.set("x2",str(ORIGINX+0.5*(R1+R2)*cos(pi/2-2*i*pi/30-pi/30-pi/60)))
+	n.set("y2",str(ORIGINY+0.5*(R1+R2)*sin(pi/2-2*i*pi/30-pi/30-pi/60)))
+	n.set("width","0.4064")
+	n.set("layer","1")
+	
+	o = SubElement(j,"wire")
+	o.set("x1",str(ORIGINX+(R2-0.75)*cos((pi/2)-(2*i+2)*pi/30)))
+	o.set("y1",str(ORIGINY+(R2-0.75)*sin((pi/2)-(2*i+2)*pi/30)))
+	o.set("x2",str(ORIGINX+0.5*(R1+R2)*cos(pi/2-2*i*pi/30-pi/30-pi/60)))
+	o.set("y2",str(ORIGINY+0.5*(R1+R2)*sin(pi/2-2*i*pi/30-pi/30-pi/60)))
+	o.set("width","0.4064")
+	o.set("layer","1")
+
+
+for i in range(0,30):
+	j = SubElement(signals,"signal")
+	j.set("name","N$"+str(i+61))
+	k = SubElement(j,"contactref")
+	k.set("element","LED"+str(2*i+121))
+	k.set("pad","A")
+	l = SubElement(j,"via")
+	l.set("x",str(ORIGINX+0.5*(R2+R3)*cos(pi/2-2*i*pi/30-pi/60)))
+	l.set("y",str(ORIGINY+0.5*(R2+R3)*sin(pi/2-2*i*pi/30-pi/60)))
+	l.set("extent","1-16")
+	l.set("drill","0.635")
+	m = SubElement(j,"contactref")
+	m.set("element","LED"+str(2*i+122))
+	m.set("pad","C")
+	
+	n = SubElement(j,"wire")
+	n.set("x1",str(ORIGINX+(R3-0.75)*cos((pi/2)-2*i*pi/30)))
+	n.set("y1",str(ORIGINY+(R3-0.75)*sin((pi/2)-2*i*pi/30)))
+	n.set("x2",str(ORIGINX+0.5*(R2+R3)*cos(pi/2-2*i*pi/30-pi/60)))
+	n.set("y2",str(ORIGINY+0.5*(R2+R3)*sin(pi/2-2*i*pi/30-pi/60)))
+	n.set("width","0.4064")
+	n.set("layer","1")
+	
+	o = SubElement(j,"wire")
+	o.set("x1",str(ORIGINX+(R3-0.75)*cos((pi/2)-(2*i+1)*pi/30)))
+	o.set("y1",str(ORIGINY+(R3-0.75)*sin((pi/2)-(2*i+1)*pi/30)))
+	o.set("x2",str(ORIGINX+0.5*(R2+R3)*cos(pi/2-2*i*pi/30-pi/60)))
+	o.set("y2",str(ORIGINY+0.5*(R2+R3)*sin(pi/2-2*i*pi/30-pi/60)))
+	o.set("width","0.4064")
+	o.set("layer","1")
+
+
+for i in range(0,30):
+	j = SubElement(signals,"signal")
+	j.set("name","N$"+str(i+91))
+	k = SubElement(j,"contactref")
+	k.set("element","LED"+str(2*i+1))
+	k.set("pad","C")
+	l = SubElement(j,"via")
+	l.set("x",str(ORIGINX+0.5*(R1+R2)*cos(pi/2-2*i*pi/30-pi/60)))
+	l.set("y",str(ORIGINY+0.5*(R1+R2)*sin(pi/2-2*i*pi/30-pi/60)))
+	l.set("extent","1-16")
+	l.set("drill","0.635")
+	m = SubElement(j,"contactref")
+	m.set("element","LED"+str(2*i+2))
+	m.set("pad","A")
+	
+	n = SubElement(j,"wire")
+	n.set("x1",str(ORIGINX+(R1+0.75)*cos((pi/2)-2*i*pi/30)))
+	n.set("y1",str(ORIGINY+(R1+0.75)*sin((pi/2)-2*i*pi/30)))
+	n.set("x2",str(ORIGINX+0.5*(R1+R2)*cos(pi/2-2*i*pi/30-pi/60)))
+	n.set("y2",str(ORIGINY+0.5*(R1+R2)*sin(pi/2-2*i*pi/30-pi/60)))
+	n.set("width","0.4064")
+	n.set("layer","1")
+	
+	o = SubElement(j,"wire")
+	o.set("x1",str(ORIGINX+(R1+0.75)*cos((pi/2)-(2*i+1)*pi/30)))
+	o.set("y1",str(ORIGINY+(R1+0.75)*sin((pi/2)-(2*i+1)*pi/30)))
+	o.set("x2",str(ORIGINX+0.5*(R1+R2)*cos(pi/2-2*i*pi/30-pi/60)))
+	o.set("y2",str(ORIGINY+0.5*(R1+R2)*sin(pi/2-2*i*pi/30-pi/60)))
+	o.set("width","0.4064")
+	o.set("layer","1")
+
+for i in range(0,30):
+	j = SubElement(signals,"signal")
+	j.set("name","N$"+str(i+121))
+	k = SubElement(j,"contactref")
+	k.set("element","LED"+str(2*i+62))
+	k.set("pad","A")
+	l = SubElement(j,"via")
+	l.set("x",str(ORIGINX+0.5*(R2+R3)*cos(pi/2-2*i*pi/30-pi/30-pi/60)))
+	l.set("y",str(ORIGINY+0.5*(R2+R3)*sin(pi/2-2*i*pi/30-pi/30-pi/60)))
+	l.set("extent","1-16")
+	l.set("drill","0.635")
+	m = SubElement(j,"contactref")
+	m.set("element","LED"+str((2*i+3)%60+60))
+	m.set("pad","C")
+	
+	n = SubElement(j,"wire")
+	n.set("x1",str(ORIGINX+(R2+0.75)*cos((pi/2)-(2*i+1)*pi/30)))
+	n.set("y1",str(ORIGINY+(R2+0.75)*sin((pi/2)-(2*i+1)*pi/30)))
+	n.set("x2",str(ORIGINX+0.5*(R2+R3)*cos(pi/2-2*i*pi/30-pi/30-pi/60)))
+	n.set("y2",str(ORIGINY+0.5*(R2+R3)*sin(pi/2-2*i*pi/30-pi/30-pi/60)))
+	n.set("width","0.4064")
+	n.set("layer","1")
+	
+	o = SubElement(j,"wire")
+	o.set("x1",str(ORIGINX+(R2+0.75)*cos((pi/2)-(2*i+2)*pi/30)))
+	o.set("y1",str(ORIGINY+(R2+0.75)*sin((pi/2)-(2*i+2)*pi/30)))
+	o.set("x2",str(ORIGINX+0.5*(R2+R3)*cos(pi/2-2*i*pi/30-pi/30-pi/60)))
+	o.set("y2",str(ORIGINY+0.5*(R2+R3)*sin(pi/2-2*i*pi/30-pi/30-pi/60)))
+	o.set("width","0.4064")
+	o.set("layer","1")
+
+for i in range(0,30):
+	j = SubElement(signals,"signal")
+	j.set("name","N$"+str(i+151))
+	k = SubElement(j,"contactref")
+	k.set("element","LED"+str(2*i+121))
+	k.set("pad","C")
+	l = SubElement(j,"via")
+	l.set("x",str(ORIGINX+0.5*(R3+R4)*cos(pi/2-2*i*pi/30-pi/60)))
+	l.set("y",str(ORIGINY+0.5*(R3+R4)*sin(pi/2-2*i*pi/30-pi/60)))
+	l.set("extent","1-16")
+	l.set("drill","0.635")
+	m = SubElement(j,"contactref")
+	m.set("element","LED"+str(2*i+122))
+	m.set("pad","A")
+	
+	n = SubElement(j,"wire")
+	n.set("x1",str(ORIGINX+(R3+0.75)*cos((pi/2)-2*i*pi/30)))
+	n.set("y1",str(ORIGINY+(R3+0.75)*sin((pi/2)-2*i*pi/30)))
+	n.set("x2",str(ORIGINX+0.5*(R3+R4)*cos(pi/2-2*i*pi/30-pi/60)))
+	n.set("y2",str(ORIGINY+0.5*(R3+R4)*sin(pi/2-2*i*pi/30-pi/60)))
+	n.set("width","0.4064")
+	n.set("layer","1")
+	
+	o = SubElement(j,"wire")
+	o.set("x1",str(ORIGINX+(R3+0.75)*cos((pi/2)-(2*i+1)*pi/30)))
+	o.set("y1",str(ORIGINY+(R3+0.75)*sin((pi/2)-(2*i+1)*pi/30)))
+	o.set("x2",str(ORIGINX+0.5*(R3+R4)*cos(pi/2-2*i*pi/30-pi/60)))
+	o.set("y2",str(ORIGINY+0.5*(R3+R4)*sin(pi/2-2*i*pi/30-pi/60)))
+	o.set("width","0.4064")
+	o.set("layer","1")
+
+
+
+### Signals defined above ###
 
 f = open('output.brd', 'w')
 f.write(prettify(eagle))
